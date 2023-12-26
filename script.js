2548670a40eeea9a2aac9fbb5b7a952ef6e0417d
smk627751 <smk627751@gmail.com>
2023-12-26 22:37:46 +0530

    final commit

--- script.js
+++ script.js
@@ -1,67 +1,125 @@
-var audio = document.getElementById('audio');
-var progressBar = document.getElementById('progress-bar');
-var progress = document.getElementById('current-progress');
-var thumb = document.getElementById('thumb')
-var play = document.getElementById('play')
-var prev = document.getElementById('prev')
-var next = document.getElementById('next')
-var playPause = document.getElementById('playPause')
-var song = document.getElementById('song')
-var artist = document.getElementById('artist')
-var playing = true
-var index = 0;
+let img = document.getElementById('img')
+let audio = document.getElementById('audio');
+let progressBar = document.getElementById('progress-bar');
+let progress = document.getElementById('current-progress');
+let thumb = document.getElementById('thumb')
+let play = document.getElementById('play')
+let prev = document.getElementById('prev')
+let next = document.getElementById('next')
+let playPause = document.getElementById('playPause')
+let heart = document.getElementById('add')
+let song = document.getElementById('song')
+let artist = document.getElementById('artist')
+let favorite = document.getElementById('favorites')
+let favorites = document.getElementById('favoriteList')
+let bg = document.getElementById('bg')
+let nav = document.getElementById('nav')
+let shuffle = document.getElementById('shuffle')
+let playing = true
+let index = 0;
+let songs = []
+let favoritesList = []
+
+nav.addEventListener('click',() => {
+    favorite.classList.toggle('show')
+})
 const makeRequest = async() => {
     const url = './data.json'
     const res = await fetch(url)
     songs = await res.json()
-    audio.src = songs[index]["src"]
-    song.textContent = songs[index]["song"]
-    artist.textContent = songs[index]["artist"]
+    load(songs[index])
 }
 makeRequest()
-var songs
 
+heart.addEventListener('click',() => {
+    favorites.innerHTML = ""
+    favoritesList[index] = songs[index];
+    favoritesList.forEach(favorite => {
+        let song = document.createElement('div')
+        song.className = 'details'
+        let icon = document.createElement('div')
+        icon.classList.add('icon')
+        let img = document.createElement('img')
+        img.className = 'img'
+        img.src = favorite["image"]
+        icon.appendChild(img)
+        let songName = document.createElement('span')
+        songName.textContent = favorite["title"]
+        song.append(icon,songName)
+        song.addEventListener('click',() => {
+            load(favorite)
+            audio.play()
+            playing = true
+            playbutton()
+        })
+        favorites.appendChild(song)
+    })
+})
+shuffle.addEventListener('click',() => {
+    for (let i = songs.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = songs[i];
+        songs[i] = songs[j];
+        songs[j] = temp;
+    }
+})
+function playbutton()
+{
+    playing ? playPause.className = 'fa-solid fa-pause' : playPause.className = 'fa-solid fa-play'
+}
 play.addEventListener('click',() => {
     if(playing)
     {
         audio.pause()
         playing = false
-        playPause.className = 'fa-solid fa-play'
+        playbutton() 
     }
     else
     {
         audio.play()
-        playPause.className = 'fa-solid fa-pause'
         playing = true
+        playbutton()
     }
 })
 prev.addEventListener('click',() => {
     index--
-    index = index < 0 ? 0 : index
-    console.log(index)
-    audio.src = songs[index]["src"]
-    song.textContent = songs[index]["song"]
-    artist.textContent = songs[index]["artist"]
+    index = index < 0 ? songs.length-1 : index
+    load(songs[index])
     audio.play()
+    playing = true
+    playbutton()
 })
 next.addEventListener('click',() => {
     index++
-    index > 4 ? 0 : index
-    console.log(index)
-    audio.src = songs[index]["src"]
-    song.textContent = songs[index]["song"]
-    artist.textContent = songs[index]["artist"]
+    index = index >= songs.length ? 0 : index
+    load(songs[index])
     audio.play()
+    playing = true
+    playbutton()
 })
 audio.addEventListener('timeupdate', () =>
 {
-    var value = (audio.currentTime / audio.duration) * 100
+    let value = (audio.currentTime / audio.duration) * 100
     thumb.style.left = value + '%'
     progress.style.width = value + '%'
+    if(audio.currentTime == audio.duration)
+    {
+        next.click()
+    }
 })
 progressBar.addEventListener('click', (e) => {
-    var width = progressBar.clientWidth
-    var clickX = e.clientX - progressBar.getBoundingClientRect().left
-    var percent = (clickX / width)
+    let width = progressBar.clientWidth
+    let clickX = e.clientX - progressBar.getBoundingClientRect().left
+    let percent = (clickX / width)
     audio.currentTime = percent * audio.duration
 })
+
+function load(music)
+{
+    img.src = music["image"]
+    audio.src = music["path"]
+    song.textContent = music["title"]
+    artist.textContent = music["artist"]
+    bg.style.backgroundImage = `url('${music["image"]}')`
+}
+
